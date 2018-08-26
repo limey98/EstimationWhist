6e3ae039eaff1a27d5d7f8f373804d1c7a37fe44
Stephen Langstone <stephen.a.langstone@gmail.com>
2018-08-26 18:50:02 +0100

    More work

--- js/main.js
+++ js/main.js
@@ -9,16 +9,71 @@
 require(["jquery", "ko", "player"], function ($, ko, player)
 {
     var vm = {
-        players: ko.observableArray([
-            player(true, "Player 1"),
-            player(false, "Player 2")
-        ])
+        players: ko.observableArray(),
+        playing: ko.observable(false)
     };
+
+    vm.addPlayer = function()
+    {
+        vm.players.push(player(false, "Player " + (vm.players().length + 1), vm));
+    }
 
     vm.deletePlayer = function(player)
     {
+        if (vm.players().length == 2)
+            return;
 
+        if (player.dealer())
+        {
+            vm.players()[vm.players.indexOf(player) + 1].dealer(true);
+        }
+
+        vm.players.remove(player);
     }
 
+    vm.playerUp = function(player)
+    {
+        var idx = vm.players.indexOf(player);
+
+        if (idx == 0)
+            return;
+
+        vm.players.remove(player);
+        vm.players.splice(idx - 1, 0, player);
+    }
+
+    vm.playerDown = function (player)
+    {
+        var idx = vm.players.indexOf(player);
+
+        if (idx == vm.players().length - 1)
+            return;
+
+        vm.players.remove(player);
+        vm.players.splice(idx + 1, 0, player);
+    }
+
+    vm.getSuit = function(idx)
+    {
+        switch (idx)
+        {
+            case 0:
+                return "Trump: &spades;";
+            case 1:
+                return "Trump: &hearts;";
+            case 2:
+                return "Trump: &clubs;";
+            case 3:
+                return "Trump: &diams;";
+            default:
+                return "No Trump";
+        }
+    }
+
+    vm.players.push(player(true, "Player 1", vm));
+    vm.players.push(player(false, "Player 2", vm));
+
     ko.applyBindings(vm);
+
+    window.ko = ko;
 });
--- js/player.js
+++ js/player.js
@@ -1,11 +1,19 @@
 ﻿define(["ko"], function(ko)
 {
-    return function(isDealer, name)
+    return function(isDealer, name, vm)
     {
         var self = {
             dealer: ko.observable(isDealer),
             name: ko.observable(name)
         };
+
+        self.setDealer = function()
+        {
+            if (self.dealer())
+                return;
+
+
+        }
 
         return self;
     }
